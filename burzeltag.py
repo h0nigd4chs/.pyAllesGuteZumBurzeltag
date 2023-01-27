dcfb5ae4cc6a59a28747af333760816f452ac95b
Honigdachs <123739154+h0nigd4chs@users.noreply.github.com>
2023-01-27 15:19:07 +0100

    Create burzeltag.py

---
+++ burzeltag.py
@@ -0,0 +1,11 @@
+
+#Dies ist eine Python-Datei mit dem Ziel, einen netten Geburtstagsgruß zu erstellen.
+
+#Zuerst fragen wir den Benutzer nach seinem Namen
+name = input("Bitte gib deinen Namen ein: ")
+
+#Jetzt fragen wir nach dem Geburtsdatum
+birthdate = input("Bitte gib dein Geburtsdatum ein: ")
+
+#Wir generieren jetzt einen personalisierten Geburtstagsgruß
+print("Hallo " + name + "! Alles Gute zum Geburtstag am " + birthdate + "!")
